154ef75ee54ac219db979847bc7802f4260ccb4a
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-02-01 21:02:57 -0500

    css package bulma installed, started adding code for a navbar, added auth.js, started working on part of the main app.js

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
+//import { QUERY_GET_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+
+const Navigation = () => {
+  return;
+};
+
+export default Navigation;
