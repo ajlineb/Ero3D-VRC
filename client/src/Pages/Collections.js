0c61d3a57c2d18571e477a5613895167e1fc9db7
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-02-28 00:12:03 -0500

    updating the pricing table

--- client/src/Pages/Collections.js
+++ client/src/Pages/Collections.js
@@ -39,29 +39,35 @@ export default function Collections() {
         </p>
         <br />
         <div className="flex justify-center">
-          <table className="border-collapse border border-slate-100 table-auto ">
+          <table className="border-collapse border border-slate-500 table-auto ">
             <thead>
               <tr>
-                <th className="border border-slate-500 p-1">Work Type</th>
-                <th className="border border-slate-500 p-2">Price</th>
+                <th className="border border-slate-500 bg-indigo-500 p-1">
+                  Work Type
+                </th>
+                <th className="border border-slate-500 bg-indigo-900 p-2">
+                  Price
+                </th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td class="border border-slate-700 p-2">Texture Work</td>
-                <td class="border border-slate-700 p-1">$0.00</td>
+                <td class="border border-slate-500 bg-indigo-300 p-2">
+                  Texture Work
+                </td>
+                <td class="border border-slate-500 bg-indigo-800 p-1">$0.00</td>
               </tr>
               <tr>
-                <td class="border border-slate-700 p-2">
+                <td class="border border-slate-500 bg-indigo-300 p-2">
                   Avatar Physical Changes
                 </td>
-                <td class="border border-slate-700 p-1">$0.00</td>
+                <td class="border border-slate-500 bg-indigo-800 p-1">$0.00</td>
               </tr>
               <tr>
-                <td class="border border-slate-700 p-2">
+                <td class="border border-slate-500 bg-indigo-300 p-2">
                   Blend Shapes/Shape keys
                 </td>
-                <td class="border border-slate-700 p-1">$0.00</td>
+                <td class="border border-slate-500 bg-indigo-800 p-1">$0.00</td>
               </tr>
             </tbody>
           </table>
