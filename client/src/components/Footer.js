6d62579c2733e7f4c6ba237f8343de343e065c8e
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-03-01 23:34:49 -0500

    added check system to see if user is at the bottom of the page in order to show or hide the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Footer() {
+  const [transparent, setTransParent] = useState(""); //for a scrolling animation for the header container
+
+  //checks window if scrolled down for nav bar
+  function handleScroll() {
+    console.log(document.body.scrollHeight);
+    let posY = window.scrollY + window.innerHeight;
+
+    if (posY >= document.body.clientHeight) {
+      setTransParent("footer-appear");
+    } else {
+      setTransParent("");
+    }
+  }
+  //used to create a listener on scroll
+  useEffect(() => {
+    window.addEventListener("click", handleScroll);
+  });
+
   return (
-    <div className="footer">
+    <div className={`footer ${transparent}`}>
       <div>
         <p className="closing">
           Created By "EroForce" with ❤️{" "}
