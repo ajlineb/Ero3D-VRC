d0a109ddf00c1d81887b6180d42e1ce4159f2ee1
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-03-15 21:21:51 -0400

    added base info for the avatar_author section, added a carousel component for the avatar_author section

---
+++ client/src/utils/avatar_authors.js
@@ -0,0 +1,18 @@
+//list of avatars their name, creator and where to find them.
+
+const data = [
+  {
+    avatarName: "Deira",
+    author: "MariVR",
+    notes:
+      "This is an original 3D model compatible with Unity's Humanoid format and designed for VRChat.",
+    price: "$45",
+    images: [
+      {
+        src: require("../images/BEAN.png"),
+        src2: require("../images/BEAN.png"),
+        alt: "beans",
+      },
+    ],
+  },
+];
