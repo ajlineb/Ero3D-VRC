1c4764b8a627c0031dd93a827d2a41c376b6f6e8
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-03-31 00:14:05 -0400

    fixed where the names for friends where instead of being in the helpful section

--- client/src/Pages/Friends.js
+++ client/src/Pages/Friends.js
@@ -12,14 +12,14 @@ export default function Tips() {
         <h2 className="subtitle">
           Helpful Friends on VRC for Avatar Development
         </h2>
-        <p>DawnStar</p>
-        <p>Scipi, Pure, Innocent, and Holesome Nyan</p>
-        <p>SilentWhisp</p>
         <p>helpful user *Name*</p>
       </div>
       <div className="centerInfo container">
         <h2 className="subtitle">Awesome Friends!</h2>
         <p>List of closest friends *Need to ask permission to place names*</p>
+        <p>DawnStar</p>
+        <p>Scipi, Pure, Innocent, and Holesome Nyan</p>
+        <p>SilentWhisp</p>
       </div>
       <div className="rightInfo container">
         <h2 className="subtitle">Are you a friend yet?</h2>
