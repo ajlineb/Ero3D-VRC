2948a26fbde7bf88f2effa0fa3317ae6d688c1b0
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-04-01 13:52:20 -0400

    made the apilogin a class VrchatLogin to call easier

--- client/src/utils/apiLogin.js
+++ client/src/utils/apiLogin.js
@@ -1,16 +1,26 @@
-// Step 1. We begin with creating a Configuration, which contains the username and password for authentication.
 const vrchat = require("vrchat");
-const configuration = new vrchat.Configuration({
-  username: "username",
-  password: "password",
-});
 
-// Step 2. VRChat consists of several API's (WorldsApi, UsersApi, FilesApi, NotificationsApi, FriendsApi, etc...)
-// Here we instantiate the Authentication API which is required for logging in.
-const AuthenticationApi = new vrchat.AuthenticationApi(configuration);
+class VrchatLogin {
+  // Step 1. We begin with creating a Configuration, which contains the username and password for authentication.
+  configuration() {
+    return new vrchat.Configuration({
+      username: "username",
+      password: "password",
+    });
+  }
 
-// Step 3. Calling getCurrentUser on Authentication API logs you in if the user isn't already logged in.
-AuthenticationApi.getCurrentUser().then((resp) => {
-  const currentUser = resp.data;
-  console.log(`Logged in as: ${currentUser.displayName}`);
-});
+  // Step 2. VRChat consists of several API's (WorldsApi, UsersApi, FilesApi, NotificationsApi, FriendsApi, etc...)
+  // Here we instantiate the Authentication API which is required for logging in.
+  authenticationApi() {
+    return new vrchat.AuthenticationApi(this.configuration);
+  }
+  // Step 3. Calling getCurrentUser on Authentication API logs you in if the user isn't already logged in.
+  getCurrentUser() {
+    return this.authenticationApi.getCurrentUser().then((res) => {
+      const currentUser = res.data;
+      console.log(`Logged in as: ${currentUser.displayName}`);
+    });
+  }
+}
+
+export default new VrchatLogin();
