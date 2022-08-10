fa2fc920b71199fb55acb8e566bce5400f2a3292
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-08-10 00:36:48 -0400

    adding filler content to the blender tips page

--- client/src/Pages/Blender.js
+++ client/src/Pages/Blender.js
@@ -5,6 +5,20 @@ export default function Blender() {
     <div className="content grid grid-cols-1 lg:grid-cols-1 gap-4 lg:gap-8 font-mono">
       <div className="centerInfo container">
         <h2 className="subtitle">Blender Basics!</h2>
+        <div className="flex flex-wrap justify-evenly">
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-purple-500">Tips For Sculpting</h2>
+            <p>Useful tips here!</p>
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-teal-500">Tips For UV Mapping</h2>
+            <p>Useful tips here!</p>
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-cyan-500">Tips For Parenting</h2>
+            <p>Useful tips here!</p>
+          </div>
+        </div>
       </div>
     </div>
   );
