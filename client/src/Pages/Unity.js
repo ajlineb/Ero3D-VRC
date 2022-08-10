259c6a29e8aa2bc51f953d4cfa76807eccc9ad93
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-08-10 00:39:56 -0400

    adding filler content to the unity tips page

--- client/src/Pages/Unity.js
+++ client/src/Pages/Unity.js
@@ -5,6 +5,22 @@ export default function Unity() {
     <div className="content grid grid-cols-1 lg:grid-cols-1 gap-4 lg:gap-8 font-mono">
       <div className="centerInfo container">
         <h2 className="subtitle">Unity Basics!</h2>
+        <div className="flex flex-wrap justify-evenly">
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-purple-500">Tips For Animations</h2>
+            <p>Useful tips here!</p>
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-teal-500">Tips For Physics</h2>
+            <p>Useful tips here!</p>
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl p-2 text-cyan-500">
+              Helpful Testing Software
+            </h2>
+            <p>Useful tips here!</p>
+          </div>
+        </div>
       </div>
     </div>
   );
