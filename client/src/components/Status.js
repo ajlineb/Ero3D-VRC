f68dfb70d4098deed924921037bae342caab8851
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-09-14 01:36:09 -0400

    updated commission status

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -4,11 +4,9 @@ export default function Status() {
   const precent = 10;
   return (
     <div className="m-auto pt-2">
-      <p className="pb-2 text-center font-black">
-        Current Progress on Comission
-      </p>
+      <p className="pb-2 text-center font-black">Current Commission Progress</p>
       <div className="progress-bar text-center text-xs pt-1">
-        18 of 20 parts complete
+        Comission complete!
       </div>
     </div>
   );
