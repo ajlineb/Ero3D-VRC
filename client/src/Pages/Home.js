274b5de9bc420c4dc213ce47575ed626d0da4b09
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-09-29 15:52:32 -0400

    added styling to the images and now adding the images with their webp formats

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -22,6 +22,10 @@ export default function Home() {
         <h2 className="subtitle">List of Future/To come projects</h2>
         <p>Current list of Commisions:</p>
         <p>
+          <span className="italic font-bold text-purple-600">PJBomb2</span> -
+          <span className=" font-bold text-green-400"> Complete!</span>
+        </p>
+        <p>
           <span className="italic font-bold text-indigo-400">Paus</span> -
           <span className=" font-bold text-green-400"> Complete!</span>
         </p>
@@ -40,26 +44,26 @@ export default function Home() {
       </div>
       <div className="centerInfo container">
         <h2 className="subtitle">Completed Commisions!</h2>
-        <div className="flex flex-wrap items-stretch ">
+        <div className="flex flex-wrap items-stretch grow">
           {data.map((d, index) => {
             return (
               <div
                 key={index}
                 id={d.id}
-                className="basis-1/2 ml-auto mr-auto mt-1 mb-1"
+                className="basis-1/2 ml-auto mr-auto mt-1 mb-1 pl-1 pr-1 "
               >
-                <h2 className="">{d.name}</h2>
+                <h2 className="pb-2 text-lg">{d.name}</h2>
                 <picture>
                   <source srcSet={d.src} type="image/webp"></source>
                   <source srcSet={d.src2} type="image/webp"></source>
                   <img
-                    className="pointer"
+                    className="pointer object-cover h-72 w-96 rounded-lg shadow-xl shadow-indigo-500/50"
                     src={d.src2}
                     alt={d.alt}
                     onClick={() => handleClick(d, index)}
                   ></img>
                 </picture>
-                <p>{d.description}</p>
+                <p className="pt-2 text-sm">{d.description}</p>
               </div>
             );
           })}
