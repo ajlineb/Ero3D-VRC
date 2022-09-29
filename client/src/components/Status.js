6e79795dc04ead23e7e0a725e29e4e43d1326ffc
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-09-29 14:29:18 -0400

    updating the commission bar info

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 
 export default function Status() {
   const precent = 10;
+  //when updating the completion info use "0 of 20 complete" or "Commission complete!"
   return (
     <div className="m-auto pt-2">
       <p className="pb-2 text-center font-black">Current Commission Progress</p>
       <div className="progress-bar text-center text-xs pt-1">
-        Commission Complete!
+        0 of 20 complete
       </div>
     </div>
   );
