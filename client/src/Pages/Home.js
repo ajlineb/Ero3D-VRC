54574f39984764b13f84a975a03a501d28bae45f
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-09-29 15:10:41 -0400

    updating the home page with clickable commission complete images

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../utils/images";
 import Authors from "../components/Carousel";
 import gumData from "../utils/gumRoadItems";
+import Modal from "../components/modal";
 
 export default function Home() {
   //for my own mental sanity...
   //grid grid-cols-3 is similar to bootstrap with a row and three columns
   //to add gutters you will need gap-x for the gutter amounts
+
+  //handles image lightbox
+  const [clickedImg, setClickedImg] = useState(null);
+
+  const handleClick = (pic, index) => {
+    setClickedImg(pic.src);
+  };
 
   return (
     <div className="content grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8 font-mono">
@@ -18,30 +26,44 @@ export default function Home() {
           <span className=" font-bold text-green-400"> Complete!</span>
         </p>
         <p>
-          <span className="italic font-bold text-blue-400">DawnStar</span>
+          <span className="italic font-bold text-blue-400">DawnStar</span> -
+          <span className=" font-bold text-yellow-400"> In Progress!</span>
         </p>
         <p>
-          <span className="italic font-bold text-pink-600">Foxy</span>
+          <span className="italic font-bold text-pink-600">Foxy</span> -
+          <span className=" font-bold text-slate-400"> In Que</span>
         </p>
         <p>
-          <span className="italic font-bold text-green-400">SilentWhisp</span>
+          <span className="italic font-bold text-green-400">SilentWhisp</span> -
+          <span className=" font-bold text-slate-400"> In Que</span>
         </p>
       </div>
       <div className="centerInfo container">
-        <h2 className="subtitle">Collage of Images</h2>
-        {data.map((d) => {
-          return (
-            <div id={d.id} className="collage">
-              <h4>{d.name}</h4>
-              <picture>
-                <source srcSet={d.src} type="image/webp"></source>
-                <source srcSet={d.src2} type="image/webp"></source>
-                <img src={d.src2} alt={d.alt}></img>
-              </picture>
-              <p>{d.description}</p>
-            </div>
-          );
-        })}
+        <h2 className="subtitle">Completed Commisions!</h2>
+        <div className="flex flex-wrap items-stretch ">
+          {data.map((d, index) => {
+            return (
+              <div
+                key={index}
+                id={d.id}
+                className="basis-1/2 ml-auto mr-auto mt-1 mb-1"
+              >
+                <h2 className="">{d.name}</h2>
+                <picture>
+                  <source srcSet={d.src} type="image/webp"></source>
+                  <source srcSet={d.src2} type="image/webp"></source>
+                  <img
+                    className="pointer"
+                    src={d.src2}
+                    alt={d.alt}
+                    onClick={() => handleClick(d, index)}
+                  ></img>
+                </picture>
+                <p>{d.description}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="rightInfo container">
         <h2 className="subtitle">Items By Me!</h2>
@@ -53,6 +75,9 @@ export default function Home() {
         <br />
         <Authors data={gumData} />
       </div>
+      {clickedImg && (
+        <Modal clickedImg={clickedImg} setClickedImg={setClickedImg} />
+      )}
     </div>
   );
 }
