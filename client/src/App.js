e6c417e6f6464ec447b502b610e2d9448272cf56
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-12-06 20:37:46 -0500

    updating the website for non-server/database since Heroku deleted and removed servers/databases because they are cheap

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import {
   Route,
   Routes,
   Navigate,
+  BrowserRouter,
 } from "react-router-dom";
 import "tw-elements";
 
@@ -31,32 +32,32 @@ import Unity from "./Pages/Unity";
 import Trouble from "./Pages/Trouble";
 
 //Graphql api endpoint
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
+// const httpLink = createHttpLink({
+//   uri: "/graphql",
+// });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem("id_token");
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
 
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 const App = () => {
   return (
-    <ApolloProvider client={client}>
+    <BrowserRouter>
       <Router basename="">
         <Header />
         <Routes>
@@ -71,7 +72,7 @@ const App = () => {
         </Routes>
         <Footer />
       </Router>
-    </ApolloProvider>
+    </BrowserRouter>
   );
 };
 
