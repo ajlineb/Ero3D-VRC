70a75a3057d92e84b788188b6ce2d6e6e60f77b6
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2023-01-01 21:00:27 -0500

    fixed typo in year

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -30,7 +30,7 @@ export default function Footer() {
           </a>
         </p>
         <p className="closing">
-          © 2022 -2023 <br></br>
+          © 2022 - 2023 <br></br>
           <span className="text-xs italic text-slate-400">
             Last Updated: 01/01/2023 8:31 pm EST
           </span>
