49ead2cc7e5d350a2aa5d59e92ed37b7a98c0bff
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2023-01-19 14:44:06 -0500

    updating timestamp

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -32,7 +32,7 @@ export default function Footer() {
         <p className="closing">
           © 2022 - 2023 <br></br>
           <span className="text-xs italic text-slate-400">
-            Last Updated: 01/15/2023 11:08 pm EST
+            Last Updated: 01/19/2023 2:43 pm EST
           </span>
         </p>
       </div>
